4a660a2d75a90a26a5b8cc25633cb33ea35972fa
Jari-Hermann Ernst <jari-hermann.ernst@t-online.de>
2013-08-20 10:44:26 +0200

    Initial commit t oGitHub

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+* For the brave souls who get this far: You are the chosen ones,
+* the valiant knights of programming who toil away, without rest,
+* fixing our most awful code. To you, true saviors, kings of men,
+* I say this: never gonna give you up, never gonna let you down,
+* never gonna run around and desert you. Never gonna make you cry,
+* never gonna say goodbye. Never gonna tell a lie and hurt you.
+*/
+
+//
+// Dear maintainer:
+//
+// Once you are done trying to 'optimize' this routine,
+// and have realized what a terrible mistake that was,
+// please increment the following counter as a warning
+// to the next guy:
+//
+// total_hours_wasted_here = 25
+//
+
+//When I wrote this, only God and I understood what I was doing
+//Now, God only knows
+
+// drunk, fix later
+
+// Magic. Do not touch.
+
+// I'm sorry.
+
+/*
+ * You may think you know what the following code does.
+ * But you dont. Trust me.
+ * Fiddle with it, and youll spend many a sleepless
+ * night cursing the moment you thought youd be clever
+ * enough to "optimize" the code below.
+ * Now close this file and go play with something else.
+ */
+
+//I am not sure why this works but it fixes the problem.
+
+/* Please work */
+
+// I am not sure if we need this, but too scared to delete.
+
+//Dear future me. Please forgive me.
+//I can't even begin to express how sorry I am.
+
+// I am not responsible of this code.
+// They made me write it, against my will.
+
+// I have to find a better job
+
+// hack for ie browser (assuming that ie is a browser)
+
+// TODO: Fix this.  Fix what?
+
+// Any maintenance developer who can't quote entire Monty Python
+// movies from memory has no business being a developer.
+
+// Catching exceptions is for communists
+
+// and there is where the dragon lives
+
+// TODO make this work
+
+# <magic type="voodoo">
+# </magic>
+
+# This job would be great if it wasn't for the fucking customers.
+
+//        .==.        .==.
+//       //`^\\      //^`\\
+//      // ^ ^\(\__/)/^ ^^\\
+//     //^ ^^ ^/6  6\ ^^ ^ \\
+//    //^ ^^ ^/( .. )\^ ^ ^ \\
+//   // ^^ ^/\| v""v |/\^ ^ ^\\
+//  // ^^/\/ /  `~~`  \ \/\^ ^\\
+//  -----------------------------
+/// HERE BE DRAGONS
+
+// Abandon all hope you who needs to debug this
+
+// And now, for something completely
+// different:
+class theLarch{
+
+}
+
+// This code was written by a genius so don't try to understand it with
+// your tiny little brain.
+
+// BEGIN HACK
+// END HACK: I feel dirty.
+
+//uncomment the following line if the program manager changes her mind again this week
+
+// If I from the future read this I'll back in time and kill myself.
+
+// Choose! Choose the form of the Destructor!
+// The choice is made! The Traveler has come!
+
+// He's dead, Jim!
+
+/* You are not expected to understand this. */
+
+// This code sucks.
+
+// *** AAAAAHAHAHAH!!  What is this??
+
+// but the "real" solution is much more complicated
+
+// It may be a hack, but it works.
+
+//user the force, luke
+
+// need a coffee to fix this.
+
+?>
---
+++ md5.php
@@ -0,0 +1,6 @@
+<?php
+
+
+//http://maquet.zeit-fuer-gesundheit.info/?authcode=5e843f775c3d394527fe180d47a1a89a
+echo md5('LinkZumMaquetZeitFuerGesundheitPortal');
+?>
